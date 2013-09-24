ad759829e393749319b44d185ad307eb10e0e3b6
Michael McFadyen <michael.mcfadyen@hotmail.co.uk>
2013-09-24 01:09:18 +0100

    daily commit

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -51,14 +51,73 @@ function change(next, pos){
 /************************************/
 
 
-$('.perks li').on("click", function(e) {
+$('.skill li').on("click", function(e) {
   var $this = $(this),
       $id = $this.attr('id'),
       $class = '.' + $('.info-' + $id).attr('class').replace('hide', '');
   
   $('.default').addClass('hide');
   $('.info-'+$id).removeClass('hide');
-  $('.perks li').removeClass('active');
+  $('.skill li').removeClass('active');
   $($this).addClass('active');
   $('div[class*=info]').not($class).addClass('hide');
+});
+
+$('.perks li').on("click", function(e) {
+  var $this = $(this),
+      $id = $this.attr('id'),
+      $class = '.' + $('.activity-' + $id).attr('class').replace('hide', '');
+  
+  $('.defaultact').addClass('hide');
+  $('.activity-'+$id).removeClass('hide');
+  $('.perks li').removeClass('active');
+  $($this).addClass('active');
+  $('div[class*=activity]').not($class).addClass('hide');
+});
+
+/*
+ * Url preview script 
+ * powered by jQuery (http://www.jquery.com)
+ * 
+ * written by Alen Grakalic (http://cssglobe.com)
+ * 
+ * for more info visit http://cssglobe.com/post/1695/easiest-tooltip-and-image-preview-using-jquery
+ *
+ */
+ 
+this.screenshotPreview = function(){	
+	/* CONFIG */
+		
+		xOffset = 10;
+		yOffset = 30;
+		
+		// these 2 variable determine popup's distance from the cursor
+		// you might want to adjust to get the right result
+		
+	/* END CONFIG */
+	$("a.screenshot").hover(function(e){
+		this.t = this.title;
+		this.title = "";	
+		var c = (this.t != "") ? "<br/>" + this.t : "";
+		$("body").append("<p id='screenshot'><img src='"+ this.rel +"' alt='url preview' />"+ c +"</p>");								 
+		$("#screenshot")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px")
+			.fadeIn("fast");						
+    },
+	function(){
+		this.title = this.t;	
+		$("#screenshot").remove();
+    });	
+	$("a.screenshot").mousemove(function(e){
+		$("#screenshot")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px");
+	});			
+};
+
+
+// starting the script on page load
+$(document).ready(function(){
+	screenshotPreview();
 });
